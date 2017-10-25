13a9c14fd3482e1b31072349ff23b307d11428cd
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-25 18:44:13 +0800

    +hw25 old code

---
+++ w7_hw01_hw25.java
@@ -0,0 +1,67 @@
+import java.io.*;
+
+public class w7_hw01_hw25 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("輸入運算面積的類型代號 (0:圓形 1:三角形 2:矩形 3:梯形)：");
+		int n = -1;
+		double a, b, c;
+		while(n == -1) {
+			try {
+				n = Integer.parseInt(buf.readLine());
+			} catch (Exception e) {
+				n = -1;
+			}
+			switch (n) {
+				case 0:
+					System.out.print("輸入半徑 :");
+					a = Double.parseDouble(buf.readLine());
+					System.out.println("圓形面積為："+circle(a));
+					break;
+				case 1:
+					System.out.print("第 1 個邊長 :");
+					a = Double.parseDouble(buf.readLine());
+					System.out.print("第 2 個邊長 :");
+					b = Double.parseDouble(buf.readLine());
+					System.out.print("第 3 個邊長 :");
+					c = Double.parseDouble(buf.readLine());
+					System.out.println("三角形面積為："+triangle(a, b, c));
+					break;
+				case 2:
+					System.out.print("輸入邊長 a：");
+					a = Double.parseDouble(buf.readLine());
+					System.out.print("輸入邊長 b：");
+					b = Double.parseDouble(buf.readLine());
+					System.out.println("矩形面積為："+square(a, b));
+					break;
+				case 3:
+					System.out.print("輸入上底：");
+					a = Double.parseDouble(buf.readLine());
+					System.out.print("輸入下底：");
+					b = Double.parseDouble(buf.readLine());
+					System.out.print("輸入高：");
+					c = Double.parseDouble(buf.readLine());
+					System.out.println("梯形面積為："+trapezium(a, b, c));
+					break;
+				default:
+					n = -1;
+					System.out.println("輸入錯誤！請重新輸入 ");
+					break;
+			}
+		}
+	}
+	public static double circle(double r) {
+		return r*r*3.14;
+	}
+	public static double triangle(double a, double b, double c) {
+		double s = (a+b+c) / 2;
+		return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+	}
+	public static double square(double a, double b) {
+		return a*b;
+	}
+	public static double trapezium(double a, double b, double h) {
+		return (a+b)*h/2;
+	}
+}
