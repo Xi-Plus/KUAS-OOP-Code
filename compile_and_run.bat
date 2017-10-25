7a54ee5b421212f66a72ae02a6669e1a26edeea9
xiplus <huangxuanyuxiplus@gmail.com>
2017-10-25 20:54:21 +0800

    update compile_and_run

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,6 +1,31 @@
 @echo off
+setlocal enableDelayedExpansion
+
 if not exist %~n1.class (
-	javac %1 -encoding utf8
+	echo compile because %~n1.class is not found
+	javac %~n1.java -encoding utf8
+) else (
+	for %%i in (%~n1.java) do (set javatime=%%~ti)
+	for %%i in (%~n1.class) do (set classtime=%%~ti)
+	echo java  = !javatime!
+	echo class = !classtime!
+	if !javatime! equ !classtime! (
+		for /f "tokens=*" %%a in ('forfiles /m %~n1.java /c "cmd /c echo @fdate @ftime"') do (set javatime=%%a)
+		for /f "tokens=*" %%a in ('forfiles /m %~n1.class /c "cmd /c echo @fdate @ftime"') do (set classtime=%%a)
+		echo java  = !javatime!
+		echo class = !classtime!
+		if !javatime! gtr !classtime! (
+			echo recompile because %~n1.java was changed
+			javac %~n1.java -encoding utf8
+		)
+	) else (
+		for /f %%i in ('dir /b /o:d %~n1.java %~n1.class') do set newfile=%%i
+		if "!newfile!" == "%~n1.java" (
+			echo recompile because %~n1.java was changed
+			javac %~n1.java -encoding utf8
+		)
+	)
 )
+echo --------------------------------
 java %~n1
 pause
