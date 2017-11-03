2d68ca9b6d8cdb5203820976dd399f7a3801f690
xiplus <huangxuanyuxiplus@gmail.com>
2017-11-03 13:22:03 +0800

    hw34 fix match email

--- w8_hw05_hw34.java
+++ w8_hw05_hw34.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class w8_hw05_hw34 {
 	public static void main(String[] args) throws IOException {
@@ -14,7 +16,7 @@ public class w8_hw05_hw34 {
 		while(true) {
 			System.out.print("請輸入帳號：");
 			acct = buf.readLine();
-			if (!acct.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])[A-Z]\\S{7,11}$")) {
+			if (!acct.matches("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])[A-Z]\\S{7,11}")) {
 				System.out.println("帳號不符合8-12個字元的英數符號混合");
 			} else if (Arrays.asList(accts).contains(acct)) {
 				System.out.println("帳號已被使用");
@@ -28,7 +30,7 @@ public class w8_hw05_hw34 {
 			while (true) {
 				System.out.print("請輸入密碼：");
 				pwd = buf.readLine();
-				if (!pwd.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])\\S{8,12}$")) {
+				if (!pwd.matches("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])\\S{8,12}")) {
 					System.out.println("密碼不符合8-12個字元的英數符號混合");
 				} else if (acct.equals(pwd)) {
 					System.out.println("密碼不可與帳號相同");
@@ -46,10 +48,13 @@ public class w8_hw05_hw34 {
 		}
 
 		String email;
+		Pattern p = Pattern.compile(".+@([^\\.]+).+");
+		Matcher m;
 		while (true) {
 			System.out.print("請輸入Email：");
 			email = buf.readLine();
-			if (email.matches("^.+@.+$")) {
+			m = p.matcher(email);
+			if (m.matches()) {
 				break;
 			} else {
 				System.out.println("Email格式錯誤");
@@ -58,6 +63,6 @@ public class w8_hw05_hw34 {
 
 		SimpleDateFormat df = new SimpleDateFormat("YYYY/MM/dd hh:mm:ss");
 
-		System.out.println(acct+" 你好，歡迎來自 kuas 的你，你的註冊時間是 "+df.format(new Date()));
+		System.out.println(acct+" 你好，歡迎來自 "+m.group(1)+" 的你，你的註冊時間是 "+df.format(new Date()));
 	}
 }
